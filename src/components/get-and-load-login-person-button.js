3bcd718048778359274eaf81851171ba4c133673
matibraun <matibraun@gmail.com>
2021-09-30 12:33:48 +0200

    todo ok2

---
+++ src/components/get-and-load-login-person-button.js
@@ -0,0 +1,54 @@
+import React from "react";
+import axios from "axios";
+
+import { useAuth0 } from "@auth0/auth0-react";
+import { useContext } from "react";
+
+import { PersonContext } from ".";
+
+
+
+const GetAndLoadLoginPersonButton = () => {
+  
+  const { user } = useAuth0();
+  const { email } = user;
+
+  const personsURL = "http://127.0.0.1:8000/app_flevo/persons/";
+  const personFilteredByEmailURL = personsURL + "?email=" + user.email
+
+
+  const { person, setPerson } = useContext(PersonContext)
+
+
+
+  const handleClickGetAndLoadPerson = () => {
+
+    console.log(email)
+
+
+    axios
+        .get(personFilteredByEmailURL)
+        .then((response) => {
+            console.log(response)
+            setPerson(response.data[0]);
+            alert('La persona se ha cargado exitosamente')
+
+        })
+
+        
+    
+  }
+  
+  return (
+
+      <button
+      className="btn btn-primary btn-block"
+      onClick={handleClickGetAndLoadPerson}
+      >
+      Get and Load Login Person
+      </button>
+
+  );
+};
+
+export default GetAndLoadLoginPersonButton;
---
+++ src/components/post-and-load-signup-person-button.js
@@ -0,0 +1,53 @@
+import React from "react";
+import axios from "axios";
+
+import { useAuth0 } from "@auth0/auth0-react";
+
+import { useContext } from "react";
+import { PersonContext } from ".";
+
+
+
+const PostAndLoadSignupPersonButton = () => {
+  
+  const { user } = useAuth0();
+  const { email } = user;
+
+  const personsURL = "http://127.0.0.1:8000/app_flevo/persons/";
+
+  const { person, setPerson } = useContext(PersonContext)
+
+
+
+  const handleClickPostAndLoadPerson = () => {
+
+    console.log(email)
+
+    axios
+        .post(personsURL, {
+            email: email,
+            password: 'Hola1234!',
+        })
+        .then((response) => {
+            console.log(response.data);
+            setPerson(response.data);
+            console.log(person)
+            alert('La persona ha sido creada exitosamente')
+        })
+
+    
+  }
+  
+  return (
+      
+      <button
+      className="btn btn-primary btn-block"
+      onClick={handleClickPostAndLoadPerson}
+      >
+      Post and Load Signup Person
+      </button>
+
+  );
+};
+
+export default PostAndLoadSignupPersonButton;
