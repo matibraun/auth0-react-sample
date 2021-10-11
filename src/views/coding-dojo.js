d0022273c8828a001b2f5cd3cd407a999395ddf4
matibraun <matibraun@gmail.com>
2021-10-11 13:19:33 +0200

    iframe andando coding dojo3

---
+++ src/views/coding-dojo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { IFrameForm } from "../components";
+
+const CodingDojo = () => {
+
+    const school = 'Coding Dojo'
+
+    return (
+        <IFrameForm school={school}/>
+    )
+};
+
+export default CodingDojo;
